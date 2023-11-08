f344c3a5689bae9d14dbfee93862d29d97409b77
idris ginanjar maulana <idrisgmaulana14@gmail.com>
2023-11-09 03:22:38 +0700

    add locking for user balance

--- src/main/java/com/idris/transaction/repository/UserRepository.java
+++ src/main/java/com/idris/transaction/repository/UserRepository.java
@@ -2,14 +2,17 @@ package com.idris.transaction.repository;
 
 import com.idris.transaction.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     public Optional<User> getUserByEmail(String email);
 }
--- src/main/java/com/idris/transaction/service/TransactionService.java
+++ src/main/java/com/idris/transaction/service/TransactionService.java
@@ -4,20 +4,16 @@ import com.idris.transaction.common.CommonResponse;
 import com.idris.transaction.common.StringHelper;
 import com.idris.transaction.constant.SequenceConstant;
 import com.idris.transaction.model.History;
-import com.idris.transaction.model.Sequence;
 import com.idris.transaction.model.User;
 import com.idris.transaction.payloads.request.TopUpBalanceRequest;
 import com.idris.transaction.payloads.request.TransactionRequest;
 import com.idris.transaction.payloads.response.HistoryTransactionResponse;
 import com.idris.transaction.repository.HistoryRepository;
-import com.idris.transaction.repository.SequenceRepository;
 import com.idris.transaction.repository.ServiceRepository;
 import com.idris.transaction.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,7 +41,6 @@ public class TransactionService {
         return new CommonResponse(0, "Get Balance Berhasil", Map.of("balance", user.getBalance()));
     }
 
-    @Lock(LockModeType.PESSIMISTIC_READ)
     @Transactional
     public CommonResponse<?> topUpBalance(String token, TopUpBalanceRequest request) {
         User user = jwtService.verifyToken(token);
@@ -64,7 +59,6 @@ public class TransactionService {
         return new CommonResponse(0, "Top Up Balance berhasil", Map.of("balance", user.getBalance()));
     }
 
-    @Lock(LockModeType.PESSIMISTIC_READ)
     @Transactional
     public CommonResponse<?> transaction(String token, TransactionRequest request) {
         User user = jwtService.verifyToken(token);
@@ -92,6 +86,7 @@ public class TransactionService {
         return new CommonResponse<>(102, "Service ataus Layanan tidak ditemukan", null);
     }
 
+    @Transactional(readOnly = true)
     public CommonResponse<?> historyTransaction(String token, Integer offset, Integer limit) {
         User user = jwtService.verifyToken(token);
 
